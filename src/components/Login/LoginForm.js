e17735197861cf114d9d857a4f83947564c93210
lopix320 <gabriel.lp2008@gmail.com>
2022-08-25 13:33:18 -0300

    feat(login): adicionando customHook chamado useForm

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,61 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../Forms/Button';
-import Input from '../Forms/Input';
+import React from "react";
+import { Link } from "react-router-dom";
+import useForm from "../../Hooks/useForm";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [token, setToken] = React.useState('');
+  const username = useForm();
+  console.log(username);
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({}),
     })
       .then((response) => {
         console.log(response);
         return response.json();
       })
       .then((json) => {
-        console.log(json.token);
-        setToken(json.token);
+        // console.log(json.token);
+        // setToken(json.token);
       });
   }
   return (
-    <div>
+    <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input
-          label="Usuário"
-          name="username"
-          type="text"
-          value={username}
-          setState={setUsername}
-        />
-        <Input
-          label="Senha"
-          name="password"
-          type="password"
-          value={password}
-          setState={setPassword}
-        />
+        <Input label="Usuário" name="username" type="text" {...username} />
         <Button>Entrar</Button>
-        {/* <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        /> */}
       </form>
       <Link to="/login/criar">Cadastro</Link>
-    </div>
+    </section>
   );
 };
 
