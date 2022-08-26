33c9eb41ad72ea64aff9d40b6bc0e883531b0d34
lopix320 <gabriel.lp2008@gmail.com>
2022-08-26 11:29:29 -0300

    feat(login): criando contexto global para usuario

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './components/Home';
-import Login from './components/Login/Login';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+import "./App.css";
+import { UserStorage } from "./contexts/UserContext";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
